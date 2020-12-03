d4ed04865a9564e9da13c4e19b6d324c58b072e3
vlijia stefan <vlijiastefan@gmail.com>
2020-12-03 14:58:39 +0200

    Adaugare clasa Logger
    Clasa Logger are nevoie de o interventie ulterior implementarii
    claselor de tratare a erorii si a celoralate clase concrete de tip Action.
    Am marcat prin comentarii locul unde trebuie aruncate erorile si unde trebuie
    instantiate clasele concrete.
    
    Signed-off-by: vlijia stefan <vlijiastefan@gmail.com>

---
+++ Logger.java
@@ -0,0 +1,77 @@
+package action.pack;
+
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+
+public class Logger {
+    private IAction actionObj;
+
+    private static boolean isValidPath(String path)
+    {
+        try{
+            Paths.get(path);
+        }
+        catch (InvalidPathException e)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void newAction(String option, String[] atributes)
+    {
+        if(option.equals("-help"))
+        {
+            //crate a help action
+        }
+        if(option.equals("-crawl"))
+        {
+            boolean robots = atributes[atributes.length-2].equals("-robots");
+            if(robots)
+            {
+                //create a crawl action with robots option
+            }
+            else
+            {
+                //create a crawl action without robots option
+            }
+        }
+        if(option.equals("-list"))
+        {
+            if(!isValidPath(atributes[atributes.length-1]))
+            {
+                //invalid path
+            }
+            //create a list action
+        }
+        if(option.equals("-search"))
+        {
+            if(!isValidPath(atributes[atributes.length-1]))
+            {
+                //invalid path
+            }
+            //create a search action
+        }
+        if(option.equals("-sitemap"))
+        {
+            if(!isValidPath(atributes[atributes.length-1]))
+            {
+                //invalid path
+            }
+            //create a sitemap action
+        }
+        if(option.equals("-filter"))
+        {
+            if(!isValidPath(atributes[atributes.length-1]))
+            {
+                //invalid path
+            }
+            //create a filter action
+        }
+    }
+
+    public boolean runAction()
+    {
+        return actionObj.runAction();
+    }
+}
