b77810b85ea18cf8bcdf8aacc0c6a0aae7e7b158
advanced373 <53189285+advanced373@users.noreply.github.com>
2020-12-05 01:03:36 +0200

    Implementeaza clasa FileWorker
    Issue: #8
    Implementeaza clasa FileWorker ce va fi folosita
    pentru operatiile asupra fisierelor.
    
    Signed-off-by: advanced373 <53189285+advanced373@users.noreply.github.com>

--- file_handlers/FileWork.java
+++ file_handlers/FileWork.java
@@ -15,14 +15,15 @@ abstract public class FileWork {
     /** local file path */
     private String path;
     /**
-     * Function responsoble for reading different type of data (it depends on file type)
+     * Function responsible for reading different type of data (it depends on file type)
      * @param fileName is absolute path to file
      * @return list of data (it depends on file type)
      */
     protected abstract ArrayList<String> read(String fileName) throws FileNotFoundException;
     /**
      * Function responsible for writing data to file (it depends on file type)
-     * @param value is absolute path to file
+     * @param fileName is absolute path to file
+     * @param data is data that has to be written
      */
     protected abstract void write(String fileName, String data) throws IOException;
     /**
@@ -32,7 +33,7 @@ abstract public class FileWork {
      */
     protected abstract ArrayList<String> search(String word);
     /**
-     * Function responsoble for filtering values from file (it depends on file type)
+     * Function responsible for filtering values from file (it depends on file type)
      * @param filter is a word by which we are filtering data from file
      * @return list of filtered data (it depends on file type)
      */
