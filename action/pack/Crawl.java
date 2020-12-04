0fdab748700c0eeb37b6ab99ca26e3ed79cb1a80
RoscaStefan <roscastefan3@gmail.com>
2020-12-04 22:55:34 +0200

    Update schelet clase Crawl
    Issue: #5
    Am implementat contructorii si scheletul
    functiilor principale pentru crawl.
    
    Signed-off-by: RoscaStefan <roscastefan3@gmail.com>

--- action/pack/Crawl.java
+++ action/pack/Crawl.java
@@ -1,28 +1,43 @@
 package action.pack;
 
+
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
+
+import  file_handlers.FileWork;
 
 public class Crawl extends ExternAction{
 
 
-    private List<String> seedURL =new ArrayList<>();
+    private List<String> urlsToCrawl =new ArrayList<>();
     private LinkedBlockingQueue<String> linksQueue=new LinkedBlockingQueue<>();
     private ExecutorService executorService;
     private Integer numThreads;
+    private String rootDir;
+    private Integer delay;
+    private Integer logLevel;
 
-    public Crawl(List<String> urlToCrawl, Integer numThreads) {
+    public String fileNameConf;
+    public String fileNameUrlList;
 
-    }
 
-    public Crawl(List<String> urlToCrawl) {
-
+    public Crawl(String fileNameConf, String fileNameUrlList) {
+        this.fileNameConf = fileNameConf;
+        this.fileNameUrlList = fileNameUrlList;
     }
 
     @Override
     public boolean runAction() {
+        this.execute();
         return false;
+    }
+
+    public void execute() {
+
+        this.executorService = Executors.newFixedThreadPool(this.numThreads);
     }
 }
--- action/pack/CrawlTask.java
+++ action/pack/CrawlTask.java
@@ -5,11 +5,21 @@ public class CrawlTask implements Runnable{
     private String urlToCrawl;
     private Crawl webCrawler;
 
-    
-
+    public CrawlTask(String urlToCrawl, Crawl webCrawler) {
+        this.urlToCrawl = urlToCrawl;
+        this.webCrawler = webCrawler;
+    }
 
     @Override
     public void run() {
+        try{
+            crawl();
+        }catch (Exception exception){
+
+        }
+    }
+
+    private void crawl(){
 
     }
 }
