4369769a17a442bf8ec647266e3fc8b42f9a7803
CorinaTanase <corinatanase98@gmail.com>
2020-12-13 12:44:32 +0200

    Update clasa Logger
    Issue: #28
    Au fost adaugate instantierea unui obiect de tip HelpAction si instructiunile de aruncare de exceptii.
    
    Signed-off-by: CorinaTanase <corinatanase98@gmail.com>

--- action/pack/Logger.java
+++ action/pack/Logger.java
@@ -1,5 +1,14 @@
+/*
+ * Logger
+ *
+ * Version 1.0
+ *
+ * All rights reserved.
+ */
+
 package action.pack;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
@@ -21,51 +30,40 @@ public class Logger {
         return true;
     }
 
-    public void newAction(String option, String[] atributes)
-    {
-        if(option.equals("-help"))
-        {
+    public void newAction(String option, String[] atributes) throws CrawlerException, FileNotFoundException {
+        if (option.equals("-help")) {
             //crate a help action
+            this.actionObj = new HelpAction();
         }
-        if(option.equals("Crawl"))
-        {
+        if (option.equals("Crawl")) {
             //boolean robots = atributes[atributes.length-2].equals("-robots");
             ArrayList<String> param = new ArrayList<>();
             param.add(atributes[0]);
+            this.actionObj = new Crawl("", "file.conf", "seed.txt", param);
 
-            try {
-                this.actionObj = new Crawl("", "file.conf", "seed.txt", param);
-            }
-            catch (IOException exception)
-            {
-                exception.printStackTrace();
-            }
         }
-        if(option.equals("Search"))
-        {
-            if(!isValidPath(atributes[atributes.length-1]))
-            {
+        if (option.equals("Search")) {
+            if (!isValidPath(atributes[atributes.length - 1])) {
                 //invalid path
-                System.out.println("invalid path");
+               // System.out.println("invalid path");
+                throw new CrawlerException("110","Invalid Path");
             }
-            this.actionObj=new SearchAction(atributes[0],atributes[1],atributes[2]);
+            this.actionObj = new SearchAction(atributes[1], atributes[2], atributes[3]);
         }
-        if(option.equals("Sitemap"))
-        {
-            if(!isValidPath(atributes[atributes.length-1]))
-            {
-                System.out.println("invalid path");
+        if (option.equals("Sitemap")) {
+            if (!isValidPath(atributes[atributes.length - 1])) {
+                //System.out.println("invalid path");
+                throw new CrawlerException("110","Invalid Path");
             }
-           this.actionObj=new SitemapAction(atributes[1]);
+            this.actionObj = new SitemapAction(atributes[1]);
         }
-        if(option.equals("Filter"))
-        {
-            if(!isValidPath(atributes[atributes.length-1]))
-            {
+        if (option.equals("Filter")) {
+            if (!isValidPath(atributes[atributes.length - 1])) {
                 //invalid path
-                System.out.println("invalid path");
+                //System.out.println("invalid path");
+                throw new CrawlerException("110","Invalid Path");
             }
-            this.actionObj=new FilterAction(atributes[1],atributes[2],atributes[3]);
+            this.actionObj = new FilterAction(atributes[1], atributes[2], atributes[3]);
         }
     }
 
